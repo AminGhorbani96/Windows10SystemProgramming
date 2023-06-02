14b067382e2341745e709ed9268dc142d463f656
AminGhorbani96 <GhorbaniAmin96@gmail.com>
2023-06-02 03:36:19 -0700

    Chapter 01 - Get system native information

---
+++ GetSystemInfo/SystemInfo.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+
+int main()
+{
+	// link : https://learn.microsoft.com/en-us/windows/win32/api/sysinfoapi/nf-sysinfoapi-getnativesysteminfo
+	SYSTEM_INFO si;
+	GetNativeSystemInfo(&si);
+
+	printf("dwPageSize : %d\n", si.dwPageSize);
+	printf("lpMinimumApplicationAddress : 0x%p\n", si.lpMinimumApplicationAddress);
+	printf("lpMaximumApplicationAddress : 0x%p\n", si.lpMaximumApplicationAddress);
+	printf("dwNumberOfProcessors : %d\n", si.dwNumberOfProcessors);
+	printf("dwProcessorType : %d\n", si.dwProcessorType);
+	return 0;
+
+}
